119e3c4d6e6fa5155784e28249ed0c244545cd14
baboface <baboface@gmail.com>
2022-06-24 12:55:18 +0900

    api

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,3 +1,18 @@
+import { useEffect } from "react";
+import { movieApi } from "../../../api";
+
 export const Home = () => {
+  useEffect(() => {
+    const movieData = async () => {
+      const {
+        data: { results },
+      } = await movieApi.nowPlaying();
+      console.log(results);
+      // console.log(playing);
+      // console.log(await movieApi.nowPlaying());
+    };
+    movieData();
+  }, []);
+
   return <div>Home</div>;
 };
