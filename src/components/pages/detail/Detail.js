9197a3d01f69b6ca6785fc12470fb783495886e3
baboface <baboface@gmail.com>
2022-06-30 11:41:43 +0900

    Detail

--- src/components/pages/detail/Detail.js
+++ src/components/pages/detail/Detail.js
@@ -1,3 +1,120 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { movieApi } from "../../../api";
+import { imgUrl } from "../../../constants/constant";
+import { Container } from "../../Container";
+import { Loading } from "../../Loading";
+
+const Wrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 100px;
+`;
+
+const Con = styled.div`
+  width: 48%;
+  &:nth-child(1) {
+    height: 80vh;
+  }
+`;
+
+const Title = styled.h3`
+  font-size: 50px;
+  font-weight: 700;
+  margin-bottom: 40px;
+`;
+
+const Release = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const Genres = styled.ul`
+  font-size: 20px;
+  font-weight: 600;
+  li {
+    list-style: disc;
+    margin-bottom: 5px;
+  }
+  margin: 20px 0 0 25px;
+`;
+
+const Runtime = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  margin-top: 20px;
+`;
+
+const Desc = styled.p`
+  font-size: 18px;
+  font-weight: 300;
+  line-height: 2.2rem;
+  margin-top: 30px;
+  opacity: 0.8;
+  letter-spacing: 0.5px;
+`;
+
 export const Detail = () => {
-  return <div>Detail</div>;
+  // console.log(movieApi.movieDetail(453395));
+  const [movieData, setMovieData] = useState();
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  // =>url주소에 있는 변수값을 가져옴
+  // const params = useParams();
+
+  // console.log(params.id);
+
+  useEffect(() => {
+    const detailData = async () => {
+      const { data } = await movieApi.movieDetail(id);
+      setMovieData(data);
+      setLoading(false);
+      // const { data: idData } = await movieApi.movieDetail(453395);
+      // const { data } = await movieApi.movieDetail(453395);
+      // console.log(idData.id);
+      // console.log(data);
+    };
+    detailData();
+  }, []);
+
+  console.log(movieData);
+
+  return (
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          {movieData && (
+            <Container>
+              <Wrap>
+                <Con
+                  style={{
+                    background: `url(${
+                      movieData.backdrop_path
+                        ? `${imgUrl}${movieData.backdrop_path}`
+                        : "https://i.ytimg.com/vi/C8WWqMKRYfk/maxresdefault.jpg"
+                    }) no-repeat center / cover`,
+                  }}
+                />
+                <Con>
+                  <Title>{movieData.title}</Title>
+                  <Release>개봉일: {movieData.release_date}</Release>
+                  <Runtime>{movieData.runtime}분</Runtime>
+                  <Genres>
+                    {movieData.genres.map((genre) => (
+                      <li key={genre.id}>{genre.name}</li>
+                    ))}
+                  </Genres>
+
+                  <Desc>{movieData.overview}</Desc>
+                </Con>
+              </Wrap>
+            </Container>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
